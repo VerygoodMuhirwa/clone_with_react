6b90243ba32ccb20a7319ef16e916f56ac83d2af
VerygoodMuhirwa <121917699+VerygoodMuhirwa@users.noreply.github.com>
2023-04-13 22:09:00 +0200

    Updated commit after authentication and authorisation

--- api/routes/routeControllers.js
+++
@@ -1,9 +0,0 @@
-const express=  require('express')
-const route= express.Router()
-const authorisationControllers= require('../controllers/authorisation.controller')
-const authenticationControllers= require('../controllers/authentication.controllers')
-
-
-route.post('/create',authenticationControllers.createUser);
-route.post('/login',authorisationControllers.loginUser)
-module.exports= route
